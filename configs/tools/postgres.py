2c67d7a3e36096eddb7beef8aef66bbf641fe60f
Nelson Paiva <nelsonpaiva10@gmail.com>
2025-11-23 07:49:49 -0300

    Continuação com o arquivo de conexão postgres.py

--- configs/tools/postgres.py
+++ configs/tools/postgres.py
@@ -21,3 +21,26 @@ class RDSPostgresSQLManager:
         ):
             raise ValueError("As credenciais do Banco de Dados não foram fornecidas")
         
+        self.db_name = db_name or os.getenv("DB_NAME")
+        self.db_user = db_user or os.getenv("DB_USER")
+        self.db_password = db_password or os.getenv("DB_PASSWORD")
+        self.db_host = db_host or os.getenv("DB_HOST")
+        self.db_port = db_port
+
+    def connect(self):
+        try:
+            connection = psycopg2.connect(
+                dbname = self.db_name,
+                user = self.db_user,
+                password = self.db_password,
+                host = self.db_host,
+                port = self.db_port
+            )
+            print("Conexão bem sucedida com o banco de dados PostgreSQL.")
+            return connection
+        except psycopg2.Error as e:
+            print(f"Erro ao conectar com o banco de dados PostgreSQL: {e}")
+            return None
+        
+
+        
