b4afac3bf07dcfada9fd7364869e80652d229552
Nelson Paiva <nelsonpaiva10@gmail.com>
2025-11-26 17:38:24 -0300

    Alteração do código start.py, inserir dados no arquivo regras.py e criação do arquivo __init__.py no rules

--- configs/rules/regras.py
+++ configs/rules/regras.py
@@ -1,7 +1,21 @@
 jornada = {
+    "name": "Jornada",
+    "table_area":[""],
+    "table_columns":[""],
+    "flavor": "stream",
+    "pages":["1-end"],
+    "strip_text": " ./n",
+    "password": None
 
 }
 
 redrex = {
-    
+    "name":"RedRex",
+    "table_area":[""],
+    "table_columns":[""],
+    "flavor": "stream",
+    "pages":["1-end"],
+    "strip_text": " ./n",
+    "password": None
+
 }
