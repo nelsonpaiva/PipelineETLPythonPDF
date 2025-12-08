ff51fe44324f68e97af7bf6b6cfa20593f5a4642
Nelson Paiva <nelsonpaiva10@gmail.com>
2025-12-07 23:57:15 -0300

    Atualização do arquivo bronze_fatura_jornada.sql

--- dbt_pdf_python/models/bronze/jornada/bronze_fatura_jornada.sql
+++ dbt_pdf_python/models/bronze/jornada/bronze_fatura_jornada.sql
@@ -5,14 +5,14 @@ FROM {{source("investimentos","fatura_jornada")}}
 
 WITH formatted AS (
     SELECT
-        "n_nota" as n_nota,
+        "n_nota" as n_nota, --correções de nomenclarutras
         "cv",
         "merc",
         "tipo",
-        TO_DATE("vecto", 'DDMMYYYY') AS vecto,
-        CAST("qted" AS INT) AS qted,
+        TO_DATE("vecto", 'DDMMYYYY') AS vecto, --correção de date_time
+        CAST("qted" AS INT) AS qted, -- fazendo cast(tipo de dado) de inteiro
         "mercadoria",
-        CAST(REPLACE("cotacao", ',', '.') AS DECIMAL(10, 2)) AS cotacao,
+        CAST(REPLACE("cotacao", ',', '.') AS DECIMAL(10, 2)) AS cotacao,--antes de fazer um cast(tipo de dado) para decimal, estou fazendo um Replace. Tirando o que é virgula e colocando ponto.
         TO_DATE("data_de_pregao", 'DDMMYYYY') AS data_de_pregao,
         CAST(REPLACE("txop", ',', '.') AS DECIMAL(10, 2)) AS txop
     FROM
